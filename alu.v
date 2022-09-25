59f8664ac42159d00a94e093b9c0956d404dcc85
Koushik Annareddy Sreenath <ka266@duke.edu>
2022-09-25 18:12:55 -0400

    Ander and Or

--- alu.v
+++ alu.v
@@ -8,14 +8,16 @@ module alu(data_operandA, data_operandB, ctrl_ALUopcode, ctrl_shiftamt, data_res
 	
    // YOUR CODE HERE //
 	
-	wire[31:0] w1,w2;
+	wire[31:0] w1,w2,w3,w4;
 	wire cout1,cout2;
 	wire o1,o2;
 
-	adder a1 (o1, cout1, w1, data_operandA, data_operandB, 1'b0);
-	subtractor s1 (o2, cout2, w2, data_operandA, data_operandB);
+	adder r1 (o1, cout1, w1, data_operandA, data_operandB, 1'b0);
+	subtractor r2 (o2, cout2, w2, data_operandA, data_operandB);
+	bitwiseand r3 (w3, data_operandA, data_operandB);
+	bitwiseor r4 (w4, data_operandA, data_operandB);
 	
-	mux2_1_32bit m1 (data_result,ctrl_ALUopcode[0], w1, w2 );
+	mux4_2_32bit m1 (data_result,ctrl_ALUopcode[1:0], w1, w2,w3,w4 );
 	mux1bit m2 (overflow,ctrl_ALUopcode[0],o1,o2);
 	
 endmodule
